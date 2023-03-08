43e2dc39804a05aef7490eed4fd75dbe0c51ae10
muhfarrasm <muhammadfarrasm@gmail.com>
2023-03-08 16:02:14 +0700

    first commit

---
+++ coba.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<< "Program penghitung luas persegi" << endl;
+    cout<< "================================" << endl;
+
+    float a, y, n luas,;
+
+    cout<< "Masukkan sisi: ";
+    cin>> a;
+    
+    cout<< endl;
+
+    luas = a * a;
+
+    cout<< "Luas persegi = " << luas << endl;
+    cout<< "Lanjut (y/n) = " << endl;
+     
+
+    return 0;
+}
